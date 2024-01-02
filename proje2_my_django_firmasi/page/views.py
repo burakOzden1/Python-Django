0f421fc2e32daf661b3492736ea5e806fef6f845
Burak Özden <zburakozden@gmail.com>
2024-01-02 12:08:28 +0300

    iletisim, ve hakkimizda sayfalari eklendi, baglantilari kuruldu

--- proje2_my_django_firmasi/page/views.py
+++ proje2_my_django_firmasi/page/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from random import randint
 
-def home(request):
+def home_view(request):
     context = {"platform": f"Django Platformu Kullanildi ve randint ile donen veri {randint(1, 100)}"}
     return render(request, 'page/home_page.html', context)
+
+def about_us_view(request):
+    context = {}
+    return render(request, 'page/about_us.html', context)
+
+def contact_us_view(request):
+    context = {}
+    return render(request, 'page/contact_us.html', context)
