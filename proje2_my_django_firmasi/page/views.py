50d7a0442289bb9271d8999d12d52fd87e0d13cd
Burak Özden <zburakozden@gmail.com>
2024-01-02 14:32:19 +0300

    proje2: Sayfalarin title alanlarinin dinamiklestirilmesi

--- proje2_my_django_firmasi/page/views.py
+++ proje2_my_django_firmasi/page/views.py
@@ -3,17 +3,26 @@ from django.http import HttpResponse
 from random import randint
 
 def home_view(request):
-    context = {"platform": f"Django Platformu Kullanildi ve randint ile donen veri {randint(1, 100)}"}
+    context = {}
     return render(request, 'page/home_page.html', context)
 
 def about_us_view(request):
-    context = {}
+    page_title = "Hakkımızda | "
+    context = dict(
+         page_title = page_title,
+    )
     return render(request, 'page/about_us.html', context)
 
 def vision_view(request):
-    context = {}
+    page_title = "Vizyonumuz | "
+    context = dict(
+         page_title = page_title,
+    )
     return render(request, 'page/vision.html', context)
 
 def contact_us_view(request):
-    context = {}
+    page_title = "İletişim | "
+    context = dict(
+         page_title = page_title,
+    )
     return render(request, 'page/contact_us.html', context)
