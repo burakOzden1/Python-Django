bb9955987d9885976eab9a0db45816d1887dcc2e
Burak Özden <zburakozden@gmail.com>
2024-01-02 16:56:47 +0300

    asdf

--- proje2_my_django_firmasi/page/views.py
+++ proje2_my_django_firmasi/page/views.py
@@ -6,11 +6,16 @@ FAKE_DB_PROJECTS = [
     f"https://picsum.photos/id/{id}/100/80" for id in range(21, 29)
 ]
 
+FAKE_DB_CAROUSEL = [
+    f"https://picsum.photos/id/{id}/1200/400" for id in range(24, 28)
+]
+
 def home_view(request):
     page_title = "Django"
     context = dict(
         page_title=page_title,
         FAKE_DB_PROJECTS=FAKE_DB_PROJECTS,
+        FAKE_DB_CAROUSEL=FAKE_DB_CAROUSEL,
     )
     return render(request, 'page/home_page.html', context)
 
