e272f4f110ef1bdc1c8a27b71747effd6e8292b0
Burak Özden <zburakozden@gmail.com>
2024-01-02 10:02:21 +0300

    asdf

--- proje1_hakan_yalcinkaya_django_firmasi/django_firmasi/project_context_processors.py
+++
@@ -1,7 +0,0 @@
-def global_context(request):
-    FAKE_DB_PROJECTS = [
-        f"https://picsum.photos/id/{id}/100/80" for id in range(21, 29)
-    ]
-    return dict(
-        FAKE_DB_PROJECTS=FAKE_DB_PROJECTS,
-    )
