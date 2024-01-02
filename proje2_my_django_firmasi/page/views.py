9ce6dd4291e5a3455c88de99b62b54cb915342ec
Burak Özden <zburakozden@gmail.com>
2024-01-02 15:44:13 +0300

    django tamplate language yapisi icerisinde bulunan for yapisinin projects.html sayfasi icerisinde uygulamali olarak kullanımı, bu sayede de proje gorsellerimizin daha az kod ile cagirilmasi.

--- proje2_my_django_firmasi/page/views.py
+++ proje2_my_django_firmasi/page/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from random import randint
 
+FAKE_DB_PROJECTS = [
+    f"https://picsum.photos/id/{id}/100/80" for id in range(21, 29)
+]
+
 def home_view(request):
     page_title = "Django"
     context = dict(
         page_title=page_title,
+        FAKE_DB_PROJECTS=FAKE_DB_PROJECTS,
     )
     return render(request, 'page/home_page.html', context)
 
@@ -15,6 +20,7 @@ def about_us_view(request):
     context = dict(
          page_title = page_title,
          hero_content = hero_content,
+         FAKE_DB_PROJECTS=FAKE_DB_PROJECTS,
     )
     return render(request, 'page/about_us.html', context)
 
@@ -24,6 +30,7 @@ def vision_view(request):
     context = dict(
          page_title = page_title,
          hero_content = hero_content,
+         FAKE_DB_PROJECTS=FAKE_DB_PROJECTS,
     )
     return render(request, 'page/vision.html', context)
 
@@ -31,7 +38,8 @@ def contact_us_view(request):
     page_title = "İletişim"
     hero_content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate imperdiet ligula, ac ullamcorper nibh finibus et. Nulla lacus elit, mattis vitae sapien nec, accumsan convallis felis. Sed iaculis justo congue est accumsan pretium. Etiam eget laoreet libero, nec euismod ex. Etiam a tellus et ligula lobortis condimentum et nec dolor. Cras sit amet magna magna. Sed sit amet imperdiet tellus, at dignissim velit. Vivamus consectetur mauris orci, vel feugiat arcu maximus at. Morbi quis malesuada justo, sit amet malesuada eros. Quisque pellentesque vulputate suscipit."
     context = dict(
-         page_title = page_title,
-         hero_content = hero_content,
+        page_title = page_title,
+        hero_content = hero_content,
+        FAKE_DB_PROJECTS=FAKE_DB_PROJECTS,
     )
     return render(request, 'page/contact_us.html', context)
