77892c8ae782f064ce790080f26628b48f8afb67
Burak Özden <zburakozden@gmail.com>
2024-01-03 16:07:51 +0300

    proje3: django orm query ile todo larin ekranda gosterilmesi

--- proje3_hakan_yalcinkaya_todo/todo/views.py
+++ proje3_hakan_yalcinkaya_todo/todo/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Todo
 
 def home_view(request):
-    return render(request, 'todo/todo_list.html', {})
+    # todos = Todo.objects.all()
+    # todos = Todo.objects.filter(is_active=True)
+    # todos = todos.filter(title__icontains="todo")
+
+    todos = Todo.objects.filter(
+        is_active = True,
+        # title__icontains="todo",
+    )
+    context = dict(
+        todos = todos
+    )
+    return render(request, 'todo/todo_list.html', context)
