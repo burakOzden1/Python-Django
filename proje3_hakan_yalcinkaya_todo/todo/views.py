c8a29c29fed779c8edcbf84117168eb7fbba4ee1
Burak Özden <zburakozden@gmail.com>
2024-01-03 17:38:06 +0300

    proje3: get_object_or_404 fonksiyonunun kullanimi ve her todo nun link yonlendirmeleri

--- proje3_hakan_yalcinkaya_todo/todo/views.py
+++ proje3_hakan_yalcinkaya_todo/todo/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.http import Http404
 from .models import Todo
 
 def home_view(request):
@@ -16,12 +17,21 @@ def home_view(request):
     )
     return render(request, 'todo/todo_list.html', context)
 
+# def todo_detail_view(request, id):
+#     try:
+#         todo = Todo.objects.get(pk=id)
+#         context = dict(
+#             todo=todo,
+#         )
+#         return render(request, 'todo/todo_detail.html', context)
+#     except Todo.DoesNotExist:
+#         raise Http404
+
+
 def todo_detail_view(request, id):
-    try:
-        todo = Todo.objects.get(pk=id)
-        context = dict(
-            todo=todo,
-        )
-        return render(request, 'todo/todo_detail.html', context)
-    except Todo.DoesNotExist:
-        raise Http404
+
+    todo = get_object_or_404(Todo, pk=id)
+    context = dict(
+        todo=todo,
+    )
+    return render(request, 'todo/todo_detail.html', context)
