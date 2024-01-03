32770ec7d8fd951856bcd0bf675b403a96809d4e
Burak Özden <zburakozden@gmail.com>
2024-01-03 12:34:19 +0300

    proje3: temel veritabani islemleri yapildi (bu islemler README.md dosyasi icerisine kaydedildi.)

--- proje3_hakan_yalcinkaya_todo/todo/admin.py
+++ proje3_hakan_yalcinkaya_todo/todo/admin.py
@@ -6,8 +6,8 @@ class TodoAdmin(admin.ModelAdmin):
     list_display = [
         'title',
         'is_active',
-        'created_at',
-        'updated_at',
+        # 'created_at',
+        # 'updated_at',
     ]
 
 
