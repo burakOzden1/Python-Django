0d83c72b34f434abfd8e8180e595cd8a15788d17
Burak Özden <zburakozden@gmail.com>
2024-01-03 11:29:13 +0300

    proje3: todo modelimizi olusturduk ve created_at, updated_at ekledik

--- proje3_hakan_yalcinkaya_todo/config/settings.py
+++ proje3_hakan_yalcinkaya_todo/config/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mk6@qgn_vcl-!1hlno)1p_7q*3*1kzdzf^4m3**_(v@mj0e012'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+]
 
 
 # Application definition
