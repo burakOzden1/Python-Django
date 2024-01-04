2792b27dc575911e966db19f887c4078435dee50
Burak Özden <zburakozden@gmail.com>
2024-01-04 16:39:17 +0300

    proje5: templates/todo/task_list.html dosyasini olusturarak 'Benim Todo List'im' yazisini ekrana getirmek icin gerekli duzenlemeleri yaptik.

--- proje5_my_todo/todolist/todo/views.py
+++ proje5_my_todo/todolist/todo/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+
+from .models import Task
 
 
-def home(request):
-    return HttpResponse("Working")
+class TaskList(ListView):
+    model = Task
