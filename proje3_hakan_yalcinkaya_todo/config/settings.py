f208a9e0328f33c4d44c4f8da5055addff44121c
Burak Özden <zburakozden@gmail.com>
2024-01-07 01:21:42 +0300

    proje3: Tekrar= dosya duzenleme

--- proje3_hakan_yalcinkaya_todo/config/settings.py
+++ proje3_hakan_yalcinkaya_todo/config/settings.py
@@ -169,12 +169,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # TODO: footer.html, navbar.html ve base.html yapilari olusturuldu. Bazi duzenlemeler yapildi.
 # TODO: todolarin views.py icerisinde filtrelenerek todo_list.html uzerinde gosterilmesi uzerine bir takim denemeler yapildi.
 # TODO: django shell ile bir takim islemler yapildi. objects.all() diyerek cagirdigimiz nesnelerin varsayilanlik ayarlari yapildi.
-<<<<<<< HEAD
 # TODO: todo_list.html ve todo_detail.html sayfalari olusturudu. (views.py ve urls.py dosyalarini iyi incele)
 # TODO: todo-detail.html sayfasi ile ilgili bazi duzenlemeler yapildi ve 404 hatalari eklendi.
 # TODO:
-=======
-# TODO: todo_list.html ve todo_detail.html sayfalari olusturuldu. (views.py ve urls.py dosyalarini iyi incele)
-# TODO:
-# TODO:
->>>>>>> 11c91e0803c6d876662437be42bc50c66036dcb1
